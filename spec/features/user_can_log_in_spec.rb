635b0033425d0607b924c1f05e2885e2fd306bce
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-24 14:57:30 +0000

    Written 2 tests that automatically passed to check login authentication

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -9,4 +9,20 @@ feature 'Logging in' do
     click_button 'Log in'
     expect(page).to have_content 'Signed in successfully.'
   end
+  scenario 'it should not log in a user if email is incorrect' do
+    visit '/'
+    click_link 'Log In'
+    fill_in 'user_email', with: 'tets@test.com'
+    fill_in 'user_password', with: 'testpassword'
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid Email or password.'
+  end
+  scenario 'it should not log in a user if password is incorrect' do
+    visit '/'
+    click_link 'Log In'
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: 'tetspassword'
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid Email or password.'
+  end
 end
