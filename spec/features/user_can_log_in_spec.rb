e7c21f1fdae09469f72a04f2a833de01c1c18854
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-24 14:55:14 +0000

    Passed test for a user to log in

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 feature 'Logging in' do
   scenario 'it should log in a user' do
     visit '/'
-    click_link 'Log in'
-    fill_in 'username', with: 'testuser'
-    fill_in 'password', with: 'testpassword'
-    click_button 'Submit'
-    expect(page).to have_content 'Successfully logged in.'
+    click_link 'Log In'
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: 'testpassword'
+    click_button 'Log in'
+    expect(page).to have_content 'Signed in successfully.'
   end
 end
