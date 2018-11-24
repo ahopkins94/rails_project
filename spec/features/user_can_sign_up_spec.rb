b0a42201b2dfeb12f9d5e0fac52ba62ca2d6120c
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-24 12:22:17 +0000

    Passed test, a user can sign up

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 feature 'Signing up' do
   scenario 'it should sign up a user' do
     visit '/'
-    click_button 'Sign Up'
-    fill_in 'username', with: 'ahopkins94'
-    fill_in 'email', with: 'anouskahopkins@gmail.com'
-    fill_in 'password', with: 'hello1234'
-    click_button 'Submit'
-    expect(page).to have_content 'Welcome ahopkins94'
+    click_link 'Sign Up'
+    fill_in 'user_username', with: 'ahopkins94'
+    fill_in 'user_email', with: 'anouskahopkins@gmail.com'
+    fill_in 'user_password', with: 'hello1234'
+    fill_in 'user_password_confirmation', with: 'hello1234'
+    click_button 'Sign up'
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
   end
 end
