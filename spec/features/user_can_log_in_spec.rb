2ae1914a5589693a5c8de141219795c8abd87bec
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-24 15:00:56 +0000

    Introduced web helper for logging in

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -2,27 +2,15 @@ require 'rails_helper'
 
 feature 'Logging in' do
   scenario 'it should log in a user' do
-    visit '/'
-    click_link 'Log In'
-    fill_in 'user_email', with: 'test@test.com'
-    fill_in 'user_password', with: 'testpassword'
-    click_button 'Log in'
+    log_in(email: 'test@test.com', password: 'testpassword')
     expect(page).to have_content 'Signed in successfully.'
   end
   scenario 'it should not log in a user if email is incorrect' do
-    visit '/'
-    click_link 'Log In'
-    fill_in 'user_email', with: 'tets@test.com'
-    fill_in 'user_password', with: 'testpassword'
-    click_button 'Log in'
+    log_in(email: 'tets@test.com', password: 'testpassword')
     expect(page).to have_content 'Invalid Email or password.'
   end
   scenario 'it should not log in a user if password is incorrect' do
-    visit '/'
-    click_link 'Log In'
-    fill_in 'user_email', with: 'test@test.com'
-    fill_in 'user_password', with: 'tetspassword'
-    click_button 'Log in'
+    log_in(email: 'test@test.com', password: 'tetspassword')
     expect(page).to have_content 'Invalid Email or password.'
   end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -7,3 +7,11 @@ def sign_up(username:, email:, password:)
   fill_in 'user_password_confirmation', with: password
   click_button 'Sign up'
 end
+
+def log_in(email:, password:)
+  visit '/'
+  click_link 'Log In'
+  fill_in 'user_email', with: email
+  fill_in 'user_password', with: password
+  click_button 'Log in'
+end
